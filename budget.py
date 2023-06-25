1e7dcde6a381a5bebfb51c8faf18d25926c47797
Alan Otero <65374890+Alanoterohs@users.noreply.github.com>
2023-06-25 01:18:38 -0300

    create methods

--- budget.py
+++ budget.py
@@ -1,1 +1,14 @@
-
+class Category:
+  def __init__(self, category):
+        self.category = category
+    
+  def deposit(amount, description):
+    return None
+  def withdraw(amount, description=""):
+    return False
+  def get_balance():
+    return None
+  def transfer(amount, self):
+    return False
+  def check_funds(amount):
+    return None
